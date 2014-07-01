9c24298a9175d12f7d981ba36d09a39d78897fe8
dawitelias <dawitelias14@gmail.com>
2014-07-01 13:56:49 -0400

    Created minified version of the FizzBuzz Test solution
    Version 2 (70 chars)

---
+++ fizz_minv2.js
@@ -0,0 +1,19 @@
+/* 
+Dawit Elias
+June 25, 2014
+dawitelias14@gmail.com
+
+The "Fizz-Buzz test" is an interview question designed to help filter out the 99.5% 
+of programming job candidates who can't seem to program their way out of a wet paper bag. 
+The text of the programming assignment is as follows:
+
+"Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number
+and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”."
+*/
+
+function fizzbuzz(){
+	i=0;while(i<100)console.log(++i%15?i%3?i%5?i:'Buzz':'Fizz':'FizzBuzz')
+}
+fizzbuzz()
